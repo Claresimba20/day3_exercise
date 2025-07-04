890d2785fabe256b20a659d1f603c63291e4ac17
Clare <awuorsimba@gmail.com>
2025-07-04 20:13:29 +0300

    Add initial python script for circle area calculation

---
+++ day2.py
@@ -0,0 +1,27 @@
+#age=10
+#if age < 20:
+   # print("less than 20")
+#else:
+   # print("greater or equal to 20")
+
+#age=25
+#if age < 20:
+    #print("less than 20")
+#elif age ==20:
+    #print("equal to 20")
+#else:
+    #print("greater than 20")
+
+#score=input("what is your score?")
+#scrore= int(score)
+#if score > 89:
+    #print("A")
+#elif score > 79:
+    #print("B")
+#else:
+    #print("pass")
+
+i=0
+while i<3:
+    print(i)
+    i=i+1
